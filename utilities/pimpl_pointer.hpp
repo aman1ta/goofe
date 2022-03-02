15ee3cdd409210c9536310b1723dd9e80cf7c2cb
Иван Смольянов <voideaterofficial@gmail.com>
2022-03-02 23:32:45 +0300

    Add "PimplPtr" class to utility module

---
+++ utilities/pimpl_pointer.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+
+namespace utilities {
+
+    // This code is taken from the website: www.habr.com
+    // ref: https://habr.com/ru/post/311038/
+    template<class T>
+    class PimplPtr {
+    public:
+        using ElementType = typename std::unique_ptr<T>::element_type;
+
+        PimplPtr() : p_(std::make_unique<T>()) {
+            static_assert(sizeof(T) > 0, "Probably, you forgot to declare constructor explicitly");
+        }
+        explicit PimplPtr(std::unique_ptr<T>&& p) : p_(std::move(p)) {}
+
+        PimplPtr(PimplPtr&&) noexcept = default;
+        PimplPtr& operator =(PimplPtr&&) noexcept = default;
+
+        ~PimplPtr() {
+            static_assert(sizeof(T) > 0, "Probably, you forgot to declare destructor explicitly");
+        }
+
+        const ElementType* get() const noexcept { return p_.get(); }
+        const ElementType* operator->() const noexcept { return get(); }
+        const ElementType& operator*() const noexcept { return *get(); }
+        explicit operator const ElementType* () const noexcept { return get(); }
+
+        ElementType* get() noexcept { return p_.get(); }
+        ElementType* operator->() noexcept { return get(); }
+        ElementType& operator*() noexcept { return *get(); }
+        explicit operator ElementType* () noexcept { return get(); }
+
+    private:
+        std::unique_ptr<T> p_;
+    };
+
+}
