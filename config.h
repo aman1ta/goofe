d9e818961115435ea78a20b93b11466fdba5e0ee
Иван Смольянов <voideaterofficial@gmail.com>
2022-03-02 23:28:02 +0300

    Add a config file with information about the engine

---
+++ config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+#include <string_view>
+
+namespace goofe {
+
+	constexpr std::size_t ENGINE_VERSION_MAJOR{ 0 };
+	constexpr std::size_t ENGINE_VERSION_MINOR{ 1 };
+
+	constexpr std::string_view ENGINE_NAME{ "GOOFE ENGINE" };
+
+}
